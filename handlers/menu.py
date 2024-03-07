f393757a23c91d69c7f90e9445ed877824304be5
Lucas Rafaldini Couto Silva <lucas.rafaldini@mercadolivre.com>
2024-03-07 01:07:37 -0300

    Add handlers (menus and screens)

---
+++ handlers/menu.py
@@ -0,0 +1,118 @@
+from typing import TYPE_CHECKING
+
+import pygame
+from builders.popup import PopupBuilder
+from managers.save_manager import NoGameSaved, SaveAndLoadManager
+
+from resources.texts import TextManagerInstance
+
+
+if TYPE_CHECKING:
+    from engine import Story
+
+class MenuHandler:
+    
+    def handle_main_menu(
+        story: 'Story',
+        event: pygame.event.Event,
+    ) -> None:
+        """
+        Handles the main menu events and updates the game state, active item index, and running status.
+
+        Args:
+            story (Story): The story object.
+            event (pygame.event.Event): The event to handle.
+
+        Returns:
+            None
+        """
+        match event.type:
+            case pygame.QUIT:
+                story.running = False
+            case pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_ESCAPE:
+                        story.running = False
+                    case pygame.K_UP:
+                        story.active_item_index = (story.active_item_index - 1) % len(story.menu_items)
+                    case pygame.K_DOWN:
+                        story.active_item_index = (story.active_item_index + 1) % len(story.menu_items)
+                    case pygame.K_RETURN:
+                        if (
+                            story.menu_items[story.selected_language][story.active_item_index]
+                            == TextManagerInstance.static_texts["menu_items"][
+                                story.selected_language
+                            ][-1]
+                        ):
+                            story.running = False
+                        elif (
+                            story.menu_items[story.selected_language][story.active_item_index]
+                            == TextManagerInstance.static_texts["menu_items"][
+                                story.selected_language
+                            ][2]
+                        ):
+                            story.current_game_state = story.possible_game_states.about
+                        elif (
+                            story.menu_items[story.selected_language][story.active_item_index]
+                            == TextManagerInstance.static_texts["menu_items"][
+                                story.selected_language
+                            ][3]
+                        ):
+                            story.current_game_state = story.possible_game_states.help
+                        elif (
+                            story.menu_items[story.selected_language][story.active_item_index]
+                            == TextManagerInstance.static_texts["menu_items"][
+                                story.selected_language
+                            ][0]
+                        ):
+                            story.current_game_state = story.possible_game_states.game
+
+                        elif (
+                            story.menu_items[story.selected_language][story.active_item_index]
+                            == TextManagerInstance.static_texts["menu_items"][
+                                story.selected_language
+                            ][1]
+                        ):
+                            print(f"Selected: {story.menu_items[story.selected_language][story.active_item_index]}")
+                            try:
+                                last_saved_state: str = SaveAndLoadManager.load_game()
+                                story.popup_info = PopupBuilder.init_popup(story.screen,  mode="load_success")
+                                # Wait a little bit before redirecting user to the state from the game
+                                # the user previously saved
+                                pygame.time.wait(2000)
+                                story.current_game_state = story.possible_game_states.game
+                                story.current_scene = last_saved_state
+                            except NoGameSaved as exc:
+                                print(exc.message)
+                                story.popup_info = PopupBuilder.init_popup(story.screen, mode="load_failed")
+                            
+
+    def handle_language_menu(
+        story: 'Story',
+        event: pygame.event.Event,
+    ):
+        """
+        Handles the language menu events.
+
+        Args:
+            story (Story): The story object.
+            event (pygame.event.Event): The event to handle.
+
+        Returns:
+            tuple: A tuple containing the updated current game state, active item index, and running flag.
+        """
+        match event.type:
+            case pygame.QUIT:
+                story.running = False
+            case pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_ESCAPE:
+                        story.running = False
+                    case pygame.K_UP:
+                        story.active_item_index = (story.active_item_index - 1) % len(story.languages)
+                    case pygame.K_DOWN:
+                        story.active_item_index = (story.active_item_index + 1) % len(story.languages)
+                    case pygame.K_RETURN:
+                        story.current_game_state = story.possible_game_states.main_menu
+                        story.selected_language = story.languages[story.active_item_index]
+                        story.active_item_index = 0
---
+++ handlers/screen.py
@@ -0,0 +1,152 @@
+from typing import TYPE_CHECKING
+import pygame
+from builders.popup import PopupBuilder
+from managers.save_manager import CantSaveGame, SaveAndLoadManager
+
+if TYPE_CHECKING:
+    from engine import Story
+
+class ScreenHandler:
+    """
+    A class that handles different screens in a game.
+
+    Attributes:
+        None
+
+    Methods:
+        clear_screen(screen, bg_color) -> None:
+            Clear the screen with the background color.
+
+        handle_about_screen(story, event):
+            Handle the about screen based on the given event.
+
+        handle_help_screen(story, event):
+            Handle the help screen based on the given event.
+
+        handle_game_dialogue_screen(story, event):
+            Handle the game dialogue screen based on the given event.
+
+        handle_game_choice_screen(story, event):
+            Handle the game choice screen based on the given event.
+    """
+
+    def clear_screen(screen: pygame.Surface, bg_color: tuple[int, int, int]) -> None:
+        """
+        Clear the screen with the background color.
+        """
+        screen.fill(bg_color)
+
+    def handle_about_screen(story: 'Story', event: pygame.event.Event) -> None:
+        """
+        Handles events for the about screen.
+
+        Args:
+            story (Story): The story object.
+            event (pygame.event.Event): The event object.
+
+        Returns:
+            None
+        """
+        match event.type:
+            case pygame.QUIT:
+                story.running = False
+            case pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_ESCAPE:
+                        story.current_game_state = story.possible_game_states.main_menu
+                        story.active_item_index = 0
+
+    def handle_help_screen(story: 'Story', event: pygame.event.Event) -> None:
+        """
+        Handles events for the help screen.
+
+        Args:
+            story (Story): The story object.
+            event (pygame.event.Event): The event object.
+
+        Returns:
+            None
+        """
+        match event.type:
+            case pygame.QUIT:
+                story.running = False
+            case pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_ESCAPE:
+                        story.current_game_state = story.possible_game_states.main_menu
+                        story.active_item_index = 0
+
+    def handle_game_dialogue_screen(story: 'Story', event: pygame.event.Event) -> None:
+        """
+        Handles events for the game dialogue screen.
+
+        Args:
+            story (Story): The story object.
+            event (pygame.event.Event): The event object.
+
+        Returns:
+            None
+        """
+        match event.type:
+            case pygame.QUIT:
+                story.running = False
+            case pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_ESCAPE:
+                        story.running = False
+                    case pygame.K_s:
+                        try:
+                            SaveAndLoadManager.save_game(story.current_scene)
+                            story.popup_info = PopupBuilder.init_popup(story.screen, mode="save_success")
+                        except CantSaveGame as exc:
+                            print(exc)
+                            story.popup_info = PopupBuilder.init_popup(story.screen, mode="save_error") 
+                    case pygame.K_RETURN:
+                        story.current_game_state = "in_choice"
+
+    def handle_game_choice_screen(story: 'Story', event: pygame.event.Event) -> None:
+        """
+        Handle the game choice screen based on the given event.
+
+        Args:
+            story (Story): The story object.
+            event (pygame.event.Event): The event object.
+
+        Returns:
+            None
+        """
+        match event.type:
+            case pygame.QUIT:
+                story.running = False
+            case pygame.KEYDOWN:
+                match event.key:
+                    case pygame.K_ESCAPE:
+                        story.running = False
+                    case pygame.K_s:
+                        # implement save game
+                        pass
+                    case pygame.K_UP:
+                        story.active_item_index = (story.active_item_index - 1) % len(
+                            story.choices[story.selected_language][story.current_scene]
+                        )
+                    case pygame.K_DOWN:
+                        story.active_item_index = (story.active_item_index + 1) % len(
+                            story.choices[story.selected_language][story.current_scene]
+                        )
+                    case pygame.K_RETURN:
+                        if (
+                            story.choices[story.selected_language][story.current_scene][
+                                story.active_item_index
+                            ][1]
+                            == "end_scene"
+                        ):
+                            story.running = False
+                        story.current_game_state = story.choices[
+                            story.selected_language
+                        ][story.current_scene][story.active_item_index][1]
+                        story.possible_game_states.game_state = story.choices[
+                            story.selected_language
+                        ][story.current_scene][story.active_item_index][1]
+                        story.current_scene = story.choices[story.selected_language][
+                            story.current_scene
+                        ][story.active_item_index][1]
