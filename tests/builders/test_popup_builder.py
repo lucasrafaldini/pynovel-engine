e011e06eb92fe9dbeb6b820dbc94046d53ccc4d0
Lucas Rafaldini Couto Silva <lucas.rafaldini@mercadolivre.com>
2024-03-08 10:06:16 -0300

    Add builders tests

---
+++ tests/builders/test_popup_builder.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from builders.popup import PopupBuilder
+
+
+class TestPopupBuilder(TestCase):
+    def setUp(self):
+        self.popup_builder = PopupBuilder
+
+    def test_init_popup(self):
+        pass
